1af7a3634f0e1d467ed8d49e57f8cc54ed98c9ec
banzaiaiai <drago2004@outlook.fr>
2025-05-02 12:15:07 +0200

    tous marche

--- shell_combined.c
+++ shell_combined.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #define _OPEN_SYS
 #include <ctype.h>
+#include <dirent.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <signal.h>
@@ -10,10 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
+
+#define BUFFER_SIZE 1024 // Size of a block for reading/writting
+#define _GNU_SOURCE
 
 // Structure
 /* A process is a single process.  */
@@ -46,6 +51,126 @@ int shell_terminal;
 int shell_is_interactive;
 /* The active jobs are linked into a list.  This is its head.   */
 job *first_job = NULL;
+
+// Exercice 2.1
+int copyFile(const char *source, const char *target) {
+  // Descriptors of the input and output file
+  int sourceDescriptor = open(source, O_RDONLY);
+  int targetDescriptor = open(target, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+  // Variable to store the content of the file
+  char buffer[BUFFER_SIZE];
+  struct stat sourceAccessControl;
+  struct stat targetAccessControl;
+  ssize_t bytesRead, bytesWritten;
+
+  // Verify that you can write in target file
+  stat(target, &targetAccessControl);
+  if (!(targetAccessControl.st_mode & S_IWUSR)) {
+    perror("You can't write the target file");
+    return EXIT_FAILURE;
+  }
+
+  // Read the source file by block and write in the target file
+  while ((bytesRead = read(sourceDescriptor, buffer, BUFFER_SIZE)) > 0) {
+    bytesWritten = write(targetDescriptor, buffer, bytesRead);
+    if (bytesWritten == -1) {
+      perror("Error during writting in target file");
+      close(sourceDescriptor);
+      close(targetDescriptor);
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (bytesRead == -1) {
+    perror("Error during the reading of source file");
+    close(sourceDescriptor);
+    close(targetDescriptor);
+    return EXIT_FAILURE;
+  }
+
+  // Closing files
+  close(sourceDescriptor);
+  close(targetDescriptor);
+
+  if (stat(source, &sourceAccessControl) == -1) {
+    perror("Error while getting the access of the source file");
+    return EXIT_FAILURE;
+  }
+  chmod(target, sourceAccessControl.st_mode);
+
+  return EXIT_SUCCESS;
+}
+
+int copyDirectory(const char *source, const char *target) {
+  struct dirent *entry;
+  struct stat fileStat;
+  struct stat sourceAccessControl;
+  char sourcePath[1024];
+  char targetPath[1024];
+
+  // Determines wether the path is related to a dir or to a file
+  if (stat(source, &sourceAccessControl) == -1) {
+    perror("Error while getting the access of the source directory");
+    return EXIT_FAILURE;
+  }
+  // Check if the path is a regular file
+  if (S_ISREG(sourceAccessControl.st_mode)) {
+    return copyFile(source, target);
+  }
+
+  DIR *sourceDirectory = opendir(source);
+  if (sourceDirectory == NULL) {
+    perror("Can't open input directory");
+    return EXIT_FAILURE;
+  }
+
+  DIR *targetDirectory = opendir(target);
+  if (targetDirectory == NULL) {
+    mkdir(target, 0755);
+    targetDirectory = opendir(target);
+    if (targetDirectory == NULL) {
+      perror("Can't copy the directory");
+      return EXIT_FAILURE;
+    }
+  }
+
+  while ((entry = readdir(sourceDirectory)) != NULL) {
+    // Ignore the two entries "." and ".."
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+
+    // Concatenate the full path of the source file
+    snprintf(sourcePath, sizeof(sourcePath), "%s/%s", source, entry->d_name);
+
+    // verify if the source file is the right one
+    if (stat(sourcePath, &fileStat) == -1) {
+      perror("Erreur lors de la récupération des informations sur le fichier");
+      closedir(sourceDirectory);
+      return EXIT_FAILURE;
+    }
+
+    // Concatenate the full path of the target file
+    snprintf(targetPath, sizeof(targetPath), "%s/%s", target, entry->d_name);
+
+    // Copy the file
+    if (copyDirectory(sourcePath, targetPath) != EXIT_SUCCESS) {
+      fprintf(stderr, "Échec de la copie du fichier %s vers %s\n", sourcePath,
+              targetPath);
+    } else {
+      printf("Copie réussie de %s vers %s\n", sourcePath, targetPath);
+    }
+  }
+
+  closedir(sourceDirectory);
+  closedir(targetDirectory);
+
+  // Copy the acces control of the source directory to the target one
+  chmod(target, sourceAccessControl.st_mode);
+
+  return EXIT_SUCCESS;
+}
 
 void format_job_info(job *j, const char *status) {
   fprintf(stderr, "%ld (%s): %s\n", (long)j->pgid, status, j->command);
@@ -233,8 +358,15 @@ void launch_process(process *p, pid_t pgid, int infile, int outfile,
   }
 
   /* Exec the new process.  Make sure we exit.  */
-  printf("%s", p->argv[0]);
-  execvp(p->argv[0], p->argv);
+
+  if (strncmp(p->argv[0], "cp", 2) == 0) {
+    printf("réussi\n");
+    printf("%s\n", p->argv[1]);
+    printf("%s\n", p->argv[2]);
+    copyDirectory(p->argv[1], p->argv[2]);
+  } else {
+    execvp(p->argv[0], p->argv);
+  }
 
   perror("execvp");
   exit(1);
